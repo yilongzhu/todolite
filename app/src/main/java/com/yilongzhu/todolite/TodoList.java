a43e3e0a195a5018d741b3c465352752369281ec
Yilong <yilong.zhu@emory.edu>
2018-08-10 21:56:03 -0400

    first commit

---
+++ app/src/main/java/com/yilongzhu/todolite/TodoList.java
@@ -0,0 +1,13 @@
+package com.yilongzhu.todolite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TodoList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+    }
+}
